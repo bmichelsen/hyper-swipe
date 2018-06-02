fd73c22ee9065c874bafae37205c8658f53cc2a9
Bjørn Michelsen <bjorn@bmichelsen.no>
2018-06-02 03:44:51 +0200

    Add minimum HTML and styles for cards

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="hello"></div>
+  <div class="hello">
+    <div class="container">
+      <div class="cards like">LIKE</div>
+      <div class="cards deck">DECK</div>
+      <div class="cards dislike">DISLIKE</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +19,26 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.container {
+  align-items: center;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  margin-top: 50px;
+}
+
+.container > .cards {
+  background: lightblue;
+  flex: 0 0 300px;
+  height: 400px;
+  margin: 0 75px;
+}
+
+.container .like,
+.container .dislike {
+  background: beige;
+  flex: 0 0 195px;
+  height: 260px;
+}
+</style>
