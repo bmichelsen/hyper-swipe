352603bc52706a53acb7878740db6fd54b899d5d
Bjørn Michelsen <bjorn@bmichelsen.no>
2018-06-02 03:52:50 +0200

    Export default directly

--- src/api/cards.js
+++ src/api/cards.js
@@ -1,11 +1,9 @@
 let image = require("../assets/logo.png")
 
-const cards = [
+export default [
   {"id":1,"title":"Friday candy","body":"Do you like to indulge in the sweetness of candy after a hard week of coding?","image":image,"status":null},
   {"id":2,"title":"Extra vacation week","body":"When the client is off, we are off. Therefore you get an extra vacation week to use during Easter and Christmas.","image":image,"status":null},
   {"id":3,"title":"Flexible work locations","body":"Some times HK (hyttekontor) is more tempting then busy city streets.","image":image,"status":null},
   {"id":4,"title":"Keep learning","body":"Everybody has to keep learning, so you have our full support to go to any conference you want.","image":image,"status":null},
   {"id":5,"title":"Personal chef","body":"Ok, he might not be personal. But our house chef will make you lunch every day of the week.","image":image,"status":null}
 ]
-
-export default cards
